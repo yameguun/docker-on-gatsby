567a4e61f8ba08f700d31bd2066b85bbc3842c78
Seiji Tanaka <nsy999jp@gmail.com>
2021-11-13 23:04:49 +0900

    Aboutページ追加

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,7 +1,7 @@
 import React from "react"
 
-import Header from "../components/header.js"
-import Footer from "../components/footer.js"
+import Header from "../components/header"
+import Footer from "../components/footer"
 import "./layout.css"
 
 export default function Layout({ children }) {
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@ import React from "react"
 import { graphql } from "gatsby"
 import Img from "gatsby-image"
 
-import Layout from "../components/layout.js"
+import Layout from "../components/layout"
 
 export default function Home({ data }) {
   return (
